299692e35aab74bd67d72b3399b635de2e0c54af
Nick Renteria <Nick Renteria>
2020-09-15 15:21:24 -0400

    better filter styles; added filter form fields

--- src/components/page/Filters.jsx
+++ src/components/page/Filters.jsx
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import FilterForm from "./FilterForm";
+
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 
@@ -10,10 +12,12 @@ function Filters() {
     }
 
     return  <div className="filter-cont">
-                <div onClick={handleExpandClick} className="filter-expand-btn">
+                <div onClick={handleExpandClick} id="filter-expand-btn" className={isExpanded && "btn-expanded"}>
                     <h3>Filters {isExpanded ? <RemoveCircleOutlineIcon /> : <AddCircleOutlineIcon />}</h3>
                 </div>
-                <div id="filters-options-cont" className={isExpanded ? "filters-options-cont-showing" : "filters-options-cont-hidden"}></div>
+                <div id="filters-options-cont" className={isExpanded ? "filters-options-cont-showing" : "filters-options-cont-hidden"}>
+                    <FilterForm />
+                </div>
             </div>
 }
 
